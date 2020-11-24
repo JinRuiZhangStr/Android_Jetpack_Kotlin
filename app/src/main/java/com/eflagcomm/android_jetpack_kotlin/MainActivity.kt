8dfcae0014e9872196a883d35185b89f84b9492d
zhangjinrui <zhang.jinrui@eflagcomm.com>
2020-11-24 17:52:43 +0800

    Android_Jetpack_Kotlin  - add recyclerview adapter click listener

--- app/src/main/java/com/eflagcomm/android_jetpack_kotlin/MainActivity.kt
+++ app/src/main/java/com/eflagcomm/android_jetpack_kotlin/MainActivity.kt
@@ -2,7 +2,11 @@ package com.eflagcomm.android_jetpack_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.listener.OnItemClickListener
+import com.eflagcomm.android_jetpack_kotlin.R.color.purple_700
 import com.eflagcomm.android_jetpack_kotlin.adapter.CatalogAdapter
 import com.eflagcomm.android_jetpack_kotlin.model.CatalogModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     private fun initView() {
         toolbar.title = "目录"
         toolbar.setTitleTextColor(resources.getColor(R.color.white))
-        toolbar.setBackgroundColor(resources.getColor(R.color.purple_700))
+        toolbar.setBackgroundColor(resources.getColor(purple_700))
         //设置导航图标要在 setSupportActionBar 之后
 //        setSupportActionBar(toolbar)
         toolbar.navigationIcon = resources.getDrawable(R.mipmap.ic_catalog)
@@ -31,6 +35,59 @@ class MainActivity : AppCompatActivity() {
         recy.layoutManager = dataLayoutManager
         var catalogAdapter = CatalogAdapter(R.layout.item_catalog,catalogList)
         recy.adapter = catalogAdapter
+
+        catalogAdapter.setOnItemClickListener(object :OnItemClickListener{
+            override fun onItemClick(adapter: BaseQuickAdapter<*, *>, view: View, position: Int) {
+
+                when (position) {
+
+                    0 -> {
+
+                    }
+
+                    1 -> {
+
+                    }
+
+                    2 -> {
+
+                    }
+
+                    3 -> {
+
+                    }
+
+                    4 -> {
+
+                    }
+
+                    5 -> {
+
+                    }
+
+                    6 -> {
+
+                    }
+
+                    7 -> {
+
+                    }
+
+                    8 -> {
+
+                    }
+
+                    9 -> {
+
+                    }
+
+                }
+
+
+            }
+
+        })
+
     }
 
 }
